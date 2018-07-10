cfdc8937d515a76b0d5b48ac23199cb14463bffd
Juan Aleman <juan.j.aleman@gmail.com>
2018-07-10 15:01:23 -0500

    Josue's example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  todoInput: string = "";
+
+  todos = [];
+
+  checkOffTodo(todo) {
+    todo.isChecked = (todo.isChecked) ? false:true;
+  }
+
+  createTodo() {
+    let trimmedInput  = this.todoInput.trim();
+    if(this.todoInput != ""){
+      this.todos.push({
+        isChecked: false,
+        name: this.todoInput,
+      });
+    }
+    console.log(this.todos);
+    this.todoInput = "";
+  }
+
+  editTodo(todo) {
+    let index = this.todos.indexOf(todo);
+    console.log(index);
+   
+    let tempTodoDesc = this.todos[index].name;
+    this.todos[index].name = prompt("Please edit your todo", this.todos[index.name]);
+    if (this.todos[index].name == null){
+      this.todos[index].name = tempTodoDesc;
+    }
+    
+  }
+
+  deleteTodo(todo) {
+    let index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+  }
+
+  deleteAllTodos() {
+    this.todos = [];
+  }
 }
